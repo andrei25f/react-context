1d1cde870ea65b18e247f4b1aa884547b065b66d
Andrey <andrey25f@gmail.com>
2024-09-19 10:26:35 +0200

    import changeStats

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Avatar from './Avatar'
 import { useDispatch, useSelector } from 'react-redux';
+import { changeStats } from '../actions/statsAction';
 
 const Stats = () => {
     const { followers, following } = useSelector(state => state.stats);
